4995779efa4187d8894cf41b517bc6b5305b62bb
Kevin <kdalt412@gmail.com>
2020-05-11 17:43:40 -0700

    Convolutional Neural Network
    Yup

---
+++ models/evolutionary strategies/CNN.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2D(1, 4, kernel_size=3, stride = 1, padding = 1)
+        self.pool = nn.MaxPool2d(3,1)
+        self.conv2 = nn.Conv2D(4, 64, kernel_size=3, stride = 1, padding = 1)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+## the output of our evolutionary strategies needs to be equal
+## to the input to the convolutional neural network
